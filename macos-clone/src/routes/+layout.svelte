5665cf8393f0d4634e809de1ea3861b4cc7a8513
Shaswat <kaushikchoudhary33@icloud.com>
2025-10-28 19:13:33 +0530

    day 2 complete

--- macos-clone/src/routes/+layout.svelte
+++ macos-clone/src/routes/+layout.svelte
@@ -4,18 +4,24 @@
   import favicon from '$lib/assets/favicon.svg';
   import Dock from '$lib/Components/Dock/Dock.svelte';
   import Desktop from '$lib/Components/desktop/desktop.svelte';
+  import Loadingscreen from '$lib/Components/loading/loadingscreen.svelte';
+  
+  let booted = false; 
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<main class="relative min-h-screen overflow-hidden">
-  <!-- TopBar is always visible -->
-  <TopBar /> 
-  <Desktop />
-   <Dock />
-
-  <!-- Render page-specific content here -->
-  <slot />
+<main class="relative min-h-screen overflow-hidden bg-black">
+    {#if !booted}
+    <Loadingscreen onFinish={() => (booted = true)} />
+  {/if}
+  
+  {#if booted}
+    <TopBar /> 
+    <Desktop />
+    <Dock />
+    <slot />
+  {/if}
 </main>
